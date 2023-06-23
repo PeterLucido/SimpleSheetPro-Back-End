e5de800cb935f8abea61d2ee6fb8b08a40eef1d1
Peter Lucido <petermlucido@gmail.com>
2023-06-23 15:50:14 -0500

    add the dive sheet schema

---
+++ models/diveSheet.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const DiveSheetSchema = new Schema({
+  title: String,
+  dives: [{ type: Schema.Types.ObjectId, ref: 'Dive' }],
+  diver: { type: Schema.Types.ObjectId, ref: 'Diver' },
+  is11Dive: { type: Boolean, default: false },
+}, {
+  timestamps: true,
+});
+
+const DiveSheet = mongoose.model('DiveSheet', DiveSheetSchema)
+
+export default DiveSheet
