308917016da9928863195c94ab3c9c63bbb7849c
Nicholas Manning <njmanning212@gmail.com>
2023-07-12 13:50:25 -0500

    adjust index API call for dive data as well as in server js

--- controllers/dives.js
+++ controllers/dives.js
@@ -2,37 +2,11 @@
 
 import diveData from '../data/diveData.js';
 
-export const index = (req, res) => {
-  let { diveNumber, dive, position, dd } = req.query;
-  let filteredDives = diveData;
-  
-  // Trim and lowercase query parameters if they are provided
-  if (diveNumber) {
-    diveNumber = diveNumber.trim();
+export const index = async (req, res) => {
+  try {
+    res.status(200).json(diveData);
+  } catch {
+    console.log(err)
+    res.status(400).json({ err: 'bad request' })
   }
-  if (dive) {
-    dive = dive.trim().toLowerCase();
-  }
-  if (position) {
-    position = position.trim().toLowerCase();
-  }
-  if (dd) {
-    dd = dd.trim().toLowerCase();
-  }
-
-  // Filter by diveNumber, dive, position and dd, if they are provided
-  if (diveNumber) {
-    filteredDives = filteredDives.filter(dive => dive.diveNumber.includes(diveNumber));
-  }
-  if (dive) {
-    filteredDives = filteredDives.filter(dive => dive.dive.toLowerCase().includes(dive));
-  }
-  if (position) {
-    filteredDives = filteredDives.filter(dive => dive.position.toLowerCase() === position);
-  }
-  if (dd) {
-    filteredDives = filteredDives.filter(dive => dive.dd.toLowerCase() === dd);
-  }
-  
-  res.json(filteredDives);
-};
+}
