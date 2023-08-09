9781e5a6b65e162528d6c633087f1dc99ead9d36
Peter Lucido <petermlucido@gmail.com>
2023-08-09 13:47:52 -0500

    adjust the schema for dives

--- models/diveSheet.js
+++ models/diveSheet.js
@@ -4,6 +4,7 @@ import diveData from '../data/diveData.js';
 const Schema = mongoose.Schema;
 
 const DiveSchema = new Schema({
+  _id: { type: String, required: true },
   diveNumber: { type: String, required: true },
   dive: { type: String, required: true },
   position: { type: String, required: true },
